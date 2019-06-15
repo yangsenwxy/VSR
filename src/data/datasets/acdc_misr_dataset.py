ffc0af0518a709b54241c5d6b3cbe55d3da49325
vic85821 <vic85821@gmail.com>
2019-06-15 16:16:28 +0800

    Update the dataset

--- src/data/datasets/acdc_misr_dataset.py
+++ src/data/datasets/acdc_misr_dataset.py
@@ -1,6 +1,5 @@
 import torch
 import numpy as np
-from numpy.fft import fftshift, ifftshift, fftn, ifftn
 from box import Box
 import nibabel as nib
 from pathlib import Path
@@ -13,19 +12,17 @@ class AcdcMISRDataset(BaseDataset):
     """ MICCAI 2017 challeng (ref: https://www.creatis.insa-lyon.fr/Challenge/acdc/index.html).
     Args:
         transforms (Box): the preprocessing and augmentation techiques applied to the training data
+        post_transforms (Box): the postprocessing techiques applied to the data after downscaling
+        degrade (Box): the downscaling process applied to the high resolution data
         num_frames (int): the number of the lr images used for super resolution
-        downscale_factor (int): the degrade factor of the lr image
     """
-    def __init__(self, transforms, num_frames, downscale_factor, **kwargs):
+    def __init__(self, transforms, post_transforms, degrade, num_frames, **kwargs):
         super().__init__(**kwargs)
-        self.data_root = self.data_root / 'training' if self.type != 'testing' else self.data_root / 'testing'
         self.transforms = compose(transforms)
+        self.post_transform = compose(post_transforms)
+        self.degrade = compose(degrade)
         self.num_frames = num_frames
-        self.downscale_factor = downscale_factor
-
-        self.degrade = Degrade(downscale_factor)
-        self.post_transform = compose([Box({'name': 'Normalize'}),
-                                       Box({'name': 'ToTensor'})])
+        self.downscale_factor = degrade[0].kwargs.downscale_factor
         self.image_path = []
         self.data = np.empty([0, 2], dtype=int)
 
@@ -47,7 +44,7 @@ class AcdcMISRDataset(BaseDataset):
 
     def __getitem__(self, index):
         patient_idx, slice_idx = self.data[index]
-        image = nib.load(str(self.image_path[patient_idx])).get_data().astype(float)
+        image = nib.load(str(self.image_path[patient_idx])).get_data().astype(np.float32)
 
         # Keep image dimension divisible by self.downscale_factor
         h, w, r = image.shape[0], image.shape[1], self.downscale_factor
@@ -56,88 +53,22 @@ class AcdcMISRDataset(BaseDataset):
         _image = image[h0:hn, w0:wn, slice_idx]
 
         # Generate the low resolution image according to the target frame
+        # - Randomly select target frame
         target_frame = np.random.randint(0, image.shape[-1])
-        hr_img = _image[..., max(0, target_frame-self.num_frames):target_frame]
-        if hr_img.shape[-1] != self.num_frames:
-            hr_img = np.concatenate((_image[..., (target_frame-self.num_frames):], hr_img), axis=-1)
-        # Convert hr_img into a list of np.array(dim: H, W, 1)
-        hr_img = hr_img.transpose((2, 0, 1))
-        hr_img = [_img[..., None] for _img in hr_img]
-        # Apply transforms
+        hr_imgs = _image[..., max(0, target_frame-self.num_frames):target_frame]
+        if hr_imgs.shape[-1] != self.num_frames:
+            hr_imgs = np.concatenate((_image[..., (target_frame-self.num_frames):], hr_imgs), axis=-1)
+        # - Convert hr_img into a list of np.array(dim: H, W, 1)
+        hr_imgs = hr_imgs.transpose((2, 0, 1))
+        hr_imgs = [_img[..., None] for _img in hr_imgs]
+        # - Apply transforms
         if self.type == 'train':
-            hr_img = self.transforms(*hr_img)
-        lr_img = self.post_transform(*self.degrade(hr_img))
-        hr_img = self.post_transform(hr_img[-1])
+            hr_imgs = self.transforms(*hr_imgs)
+        lr_imgs = self.degrade(*hr_imgs)
+        lr_imgs = self.post_transform(*lr_imgs)
+        hr_imgs = self.post_transform(*hr_imgs)
+        # - Permute the tensor to (C, H, W)
+        lr_imgs = tuple(_img.permute(2, 0, 1).contiguous() for _img in lr_imgs)
+        hr_imgs = tuple(_img.permute(2, 0, 1).contiguous() for _img in hr_imgs)
 
-        return {'hr_img': hr_img, 'lr_img': lr_img}
-
-
-class Degrade(object):
-    """ The class is used for generating low resolution images
-    Args:
-        downscale_factor (int): the degrade factor
-    """
-    def __init__(self, downscale_factor):
-        self.downscale_factor = downscale_factor
-
-    def _transform_kspace_to_image(self, k, dim=None, img_shape=None):
-        if not dim:
-            dim = range(k.ndim)
-        k = ifftshift(k, axes=dim)
-        k = ifftn(k, s=img_shape, axes=dim, norm='ortho')
-        img = fftshift(k, axes=dim)
-        img = np.abs(img)
-        img = img[..., None]
-        return img
-
-    def _transform_image_to_kspace(self, img, dim=None, k_shape=None):
-        img = img.squeeze()
-        if not dim:
-            dim = range(img.ndim)
-        img = ifftshift(img, axes=dim)
-        img = fftn(img, s=k_shape, axes=dim, norm='ortho')
-        k = fftshift(img, axes=dim)
-        return k
-
-    def __call__(self, imgs):
-        """
-        Args:
-            imgs (tuple of numpy.ndarray): The images to be down-scaled.
-
-        Returns:
-            imgs (tuple of numpy.ndarray): The degraded images.
-        """
-        if not all(isinstance(img, np.ndarray) for img in imgs):
-            raise TypeError('All of the images should be numpy.ndarray.')
-
-        if not all(img.ndim == 3 for img in imgs) and not all(img.ndim == 4 for img in imgs):
-            raise ValueError("All of the images' dimensions should be 3 (2D images) or 4 (3D images).")
-
-        _imgs = []
-        for img in imgs:
-            if img.ndim == 3:
-                # 2D image: H, W, C
-                kspace = self._transform_image_to_kspace(img)
-                kx_max = kspace.shape[0]//2
-                ky_max = kspace.shape[1]//2
-                lx = kspace.shape[0]//self.downscale_factor
-                ly = kspace.shape[1]//self.downscale_factor
-                img = self._transform_kspace_to_image(kspace[kx_max - lx // 2 : kx_max + (lx - (lx // 2)),
-                                                             ky_max - ly // 2 : ky_max + (ly - (ly // 2))])
-            elif img.ndim == 4:
-                # Need to be tested
-                # 3D image: H, W, D, C
-                slices_list = []
-                for i in range(img.shape[-2]):
-                    kspace = self._transform_image_to_kspace(img[:, :, i])
-                    kx_max = kspace.shape[0]//2
-                    ky_max = kspace.shape[1]//2
-                    # zero padding
-                    rect_func = np.zeros_like(kspace)
-                    rect_func[kx_max - kx_max//self.downscale_factor : kx_max + kx_max//self.downscale_factor,
-                              ky_max - ky_max//self.downscale_factor : ky_max + ky_max//self.downscale_factor] = 1
-                    slices_list.append(self._transform_kspace_to_image(kspace * rect_func))
-                img = np.stack(slices_list, axis=-2)
-            _imgs.append(img)
-        imgs = tuple(_imgs)
-        return imgs
+        return {'hr_imgs': hr_imgs, 'lr_imgs': lr_imgs}
