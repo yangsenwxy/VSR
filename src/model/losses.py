50d91c70f23b0f80390ba9fa1a447a91e9f4827d
peter850706 <peter850706@gmail.com>
2019-06-19 09:51:54 +0800

    Fix MISRL1Loss bugs

--- src/model/losses.py
+++ src/model/losses.py
@@ -2,13 +2,14 @@ import torch
 import torch.nn as nn
 
 
-class MISRL1Loss(nn.L1Loss):
+class MISRL1Loss(nn.Module):
     """The mean of the multple L1Loss for MISR task.
     """
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super().__init__()
+        self.l1_loss = nn.L1Loss(**kwargs)
 
     def forward(self, outputs, targets):
-        losses = [super().forward(output, target) for output, target in zip(outputs, targets)]
+        losses = [self.l1_loss(output, target) for output, target in zip(outputs, targets)]
         loss = torch.stack(losses).mean()
         return loss
