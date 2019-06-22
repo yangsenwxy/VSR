1c4ddf508e16740c59418135c23a75e07a03df13
peter850706 <peter850706@gmail.com>
2019-06-22 12:16:36 +0800

    Update comments

--- src/callbacks/loggers/acdc_sisr_logger.py
+++ src/callbacks/loggers/acdc_sisr_logger.py
@@ -11,19 +11,19 @@ class AcdcSISRLogger(BaseLogger):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def _add_images(self, epoch, train_batch, train_outputs, valid_batch, valid_outputs):
+    def _add_images(self, epoch, train_batch, train_output, valid_batch, valid_output):
         """Plot the visualization results.
         Args:
             epoch (int): The number of trained epochs.
             train_batch (dict): The training batch.
-            train_outputs (torch.Tensor): The training outputs.
+            train_output (torch.Tensor): The training output.
             valid_batch (dict): The validation batch.
-            valid_outputs (torch.Tensor): The validation outputs.
+            valid_output (torch.Tensor): The validation output.
         """
         train_hr_img = make_grid(train_batch['hr_img'], nrow=1, normalize=True, scale_each=True, pad_value=1)
-        train_sr_img = make_grid(train_outputs, nrow=1, normalize=True, scale_each=True, pad_value=1)
+        train_sr_img = make_grid(train_output, nrow=1, normalize=True, scale_each=True, pad_value=1)
         valid_hr_img = make_grid(valid_batch['hr_img'], nrow=1, normalize=True, scale_each=True, pad_value=1)
-        valid_sr_img = make_grid(valid_outputs, nrow=1, normalize=True, scale_each=True, pad_value=1)
+        valid_sr_img = make_grid(valid_output, nrow=1, normalize=True, scale_each=True, pad_value=1)
 
         train_grid = torch.cat([train_hr_img, train_sr_img], dim=-1)
         valid_grid = torch.cat([valid_hr_img, valid_sr_img], dim=-1)
