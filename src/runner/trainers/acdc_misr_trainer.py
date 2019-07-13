4a18a5583f2b48479eec0452c7c3af779da71166
peter850706 <peter850706@gmail.com>
2019-07-13 11:57:01 +0800

    Modify AcdcMISRTrainer: a template for every MISR methods

--- src/runner/trainers/acdc_misr_trainer.py
+++ src/runner/trainers/acdc_misr_trainer.py
@@ -5,8 +5,6 @@ from src.runner.trainers.base_trainer import BaseTrainer
 
 class AcdcMISRTrainer(BaseTrainer):
     """The ACDC trainer for Multi-Images Super Resolution.
-
-    Specificly, there are multiple LR inputs and corresponding HR targets.
     """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -31,30 +29,22 @@ class AcdcMISRTrainer(BaseTrainer):
         Returns:
             losses (list of torch.Tensor): The computed losses.
         """
-        losses = [loss(outputs, targets) for loss in self.losses]
-        return losses
+        raise NotImplementedError
 
     def _compute_metrics(self, outputs, targets):
         """Compute the metrics.
         Args:
-             outputs (list of torch.Tensor): The model outputs.
-             targets (list of torch.Tensor): The data targets.
+            outputs (list of torch.Tensor): The model outputs.
+            targets (list of torch.Tensor): The data targets.
 
         Returns:
             metrics (list of torch.Tensor): The computed metrics.
         """
-        outputs = list(map(self._min_max_normalize, outputs))
-        targets = list(map(self._min_max_normalize, targets))
-
-        metrics = []
-        for metric in self.metrics:
-            sequence_metrics = [metric(output, target) for output, target in zip(outputs, targets)]
-            metrics.append(torch.stack(sequence_metrics).mean())
-        return metrics
+        raise NotImplementedError
 
     @staticmethod
     def _min_max_normalize(imgs):
-        """Normalize the image to [0, 1].
+        """Normalize the images to [0, 1].
         Args:
             imgs (torch.Tensor) (N, C, H, W): Te images to be normalized.
 
