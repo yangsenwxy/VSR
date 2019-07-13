714a4ac50a88954e98a958b27121a8afc298f226
peter850706 <peter850706@gmail.com>
2019-07-13 15:08:02 +0800

    Implemet AcdcSISRSRFBTrainer

--- src/runner/trainers/__init__.py
+++ src/runner/trainers/__init__.py
@@ -1,2 +1,3 @@
-from .acdc_misr_trainer import AcdcMISRTrainer
 from .acdc_sisr_trainer import AcdcSISRTrainer
+from .acdc_sisr_srfb_trainer import AcdcSISRSRFBTrainer
+from .acdc_misr_trainer import AcdcMISRTrainer
---
+++ src/runner/trainers/acdc_sisr_srfb_trainer.py
@@ -0,0 +1,41 @@
+import torch
+
+from src.runner.trainers import AcdcSISRTrainer
+
+
+class AcdcSISRSRFBTrainer(AcdcSISRTrainer):
+    """The ACDC trainer for Single-Image Super Resolution using the SRFBNet.
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def _compute_losses(self, outputs, target):
+        """Compute the losses.
+        Args:
+            outputs (list of torch.Tensor): The model outputs.
+            target (torch.Tensor): The data target.
+
+        Returns:
+            losses (list of torch.Tensor): The computed losses.
+        """
+        losses = []
+        for loss_fn in self.loss_fns:
+            # Average the losses computed at every time steps.
+            loss = torch.stack([loss_fn(output, target) for output in outputs]).mean()
+            losses.append(loss)
+        return losses
+
+    def _compute_metrics(self, outputs, target):
+        """Compute the metrics.
+        Args:
+            outputs (list of torch.Tensor): The model outputs.
+            target (torch.Tensor): The data target.
+
+        Returns:
+            metrics (list of torch.Tensor): The computed metrics.
+        """
+        # Do the min-max normalization before computing the metric.
+        output, target = self._min_max_normalize(outputs[-1]), self._min_max_normalize(target)
+
+        metrics = [metric_fn(output, target) for metric_fn in self.metric_fns]
+        return metrics
