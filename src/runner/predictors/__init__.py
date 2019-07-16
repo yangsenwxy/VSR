d615033b8eb9fbf57a3b00aaf932bc763e8f0d44
peter850706 <peter850706@gmail.com>
2019-07-16 11:38:38 +0800

    Implement AcdcMISRSRFBPredictor

--- src/runner/predictors/__init__.py
+++ src/runner/predictors/__init__.py
@@ -1,3 +1,4 @@
 from .acdc_sisr_predictor import AcdcSISRPredictor
 from .acdc_sisr_srfb_predictor import AcdcSISRSRFBPredictor
 from .acdc_misr_predictor import AcdcMISRPredictor
+from .acdc_misr_srfb_predictor import AcdcMISRSRFBPredictor
