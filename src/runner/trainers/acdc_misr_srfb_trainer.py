be34ff6e3ba22c7a369ce77ffa65f97467485930
peter850706 <peter850706@gmail.com>
2019-07-19 21:19:03 +0800

    Modify AcdcMISRSRFBTrainer: be comparable with the SISR methods

--- src/runner/trainers/acdc_misr_srfb_trainer.py
+++ src/runner/trainers/acdc_misr_srfb_trainer.py
@@ -1,4 +1,5 @@
 import torch
+from tqdm import tqdm
 
 from src.runner.trainers import AcdcMISRTrainer
 
@@ -8,6 +9,69 @@ class AcdcMISRSRFBTrainer(AcdcMISRTrainer):
     """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+    def _run_epoch(self, mode):
+        """Run an epoch for training.
+        Args:
+            mode (str): The mode of running an epoch ('training' or 'validation').
+        Returns:
+            log (dict): The log information.
+            batch (dict or sequence): The last batch of the data.
+            outputs (torch.Tensor or sequence of torch.Tensor): The corresponding model outputs.
+        """
+        if mode == 'training':
+            self.net.train()
+        else:
+            self.net.eval()
+        dataloader = self.train_dataloader if mode == 'training' else self.valid_dataloader
+        trange = tqdm(dataloader,
+                      total=len(dataloader),
+                      desc=mode)
+
+        log = self._init_log()
+        count = 0
+        for batch in trange:
+            batch = self._allocate_data(batch)
+            inputs, targets = self._get_inputs_targets(batch)
+            if mode == 'training':
+                outputs = self.net(inputs)
+                losses = self._compute_losses(outputs, targets)
+                loss = (torch.stack(losses) * self.loss_weights).sum()
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+            else:
+                with torch.no_grad():
+                    outputs = self.net(inputs)
+                    losses = self._compute_losses(outputs, targets)
+                    loss = (torch.stack(losses) * self.loss_weights).sum()
+            metrics =  self._compute_metrics(outputs, targets)
+
+            batch_size = self.train_dataloader.batch_size if mode == 'training' else self.valid_dataloader.batch_size
+            T = len(inputs)
+            self._update_log(log, batch_size, T, loss, losses, metrics)
+            count += batch_size * T
+            trange.set_postfix(**dict((key, f'{value / count: .3f}') for key, value in log.items()))
+
+        for key in log:
+            log[key] /= count
+        return log, batch, outputs
+
+    def _update_log(self, log, batch_size, T, loss, losses, metrics):
+        """Update the log.
+        Args:
+            log (dict): The log to be updated.
+            batch_size (int): The batch size.
+            T (int): The total number of the frames.
+            loss (torch.Tensor): The weighted sum of the computed losses.
+            losses (sequence of torch.Tensor): The computed losses.
+            metrics (sequence of torch.Tensor): The computed metrics.
+        """
+        log['Loss'] += loss.item() * batch_size * T
+        for loss_fn, loss in zip(self.loss_fns, losses):
+            log[loss_fn.__class__.__name__] += loss.item() * batch_size * T
+        for metric_fn, metric in zip(self.metric_fns, metrics):
+            log[metric_fn.__class__.__name__] += metric.item() * batch_size * T
 
     def _compute_losses(self, outputs, targets):
         """Compute the losses.
