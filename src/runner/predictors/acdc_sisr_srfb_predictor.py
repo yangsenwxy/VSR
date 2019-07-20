79b65cc2a9be97f10db43fcc1e5aec3118b088d2
peter850706 <peter850706@gmail.com>
2019-07-20 17:26:21 +0800

    Modify AcdcSISRSRFBPredictor

--- src/runner/predictors/acdc_sisr_srfb_predictor.py
+++ src/runner/predictors/acdc_sisr_srfb_predictor.py
@@ -1,4 +1,11 @@
+import csv
 import torch
+import logging
+import imageio
+import numpy as np
+from scipy.misc import imsave
+from tqdm import tqdm
+from pathlib import Path
 
 from src.runner.predictors import AcdcSISRPredictor
 
@@ -9,11 +16,87 @@ class AcdcSISRSRFBPredictor(AcdcSISRPredictor):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def predict(self):
+        """The testing process.
+        """
+        self.net.eval()
+        trange = tqdm(self.test_dataloader,
+                      total=len(self.test_dataloader),
+                      desc='testing')
+
+        if self.export_prediction:
+            videos_dir = self.saved_dir / 'videos'
+            imgs_dir = self.saved_dir / 'imgs'
+            csv_path = self.saved_dir / 'results.csv'
+
+            sr_imgs = []
+            tmp_sid = None
+            header = ['name'] + \
+                     [metric_fn.__class__.__name__ for metric_fn in self.metric_fns] + \
+                     [loss_fns.__class__.__name__ for loss_fns in self.loss_fns]
+            results = [header]
+
+        log = self._init_log()
+        count = 0
+        for batch in trange:
+            batch = self._allocate_data(batch)
+            input, target, index = self._get_inputs_targets(batch)
+            with torch.no_grad():
+                outputs = self.net(input)
+                losses = self._compute_losses(outputs, target)
+                loss = (torch.stack(losses) * self.loss_weights).sum()
+                metrics = self._compute_metrics(outputs, target)
+
+                if self.export_prediction:
+                    path = self.test_dataloader.dataset.data_paths[index]
+                    filename = path.parts[-1].split('.')[0]
+                    patient, _, sid, fid = filename.split('_')
+
+                    _losses = [loss.item() for loss in losses]
+                    _metrics = [metric.item() for metric in metrics]
+                    results.append([filename, *_metrics, *_losses])
+
+                    # Save the video.
+                    if sid != tmp_sid and index != 0:
+                        output_dir = videos_dir / patient
+                        if not output_dir.is_dir():
+                            output_dir.mkdir(parents=True)
+                        video_name = tmp_sid.replace('slice', 'sequence') + '.gif'
+                        self._dump_video(sr_imgs, output_dir / video_name)
+                        sr_imgs = []
+
+                    output = self._min_max_normalize(outputs[-1]) * 255
+                    sr_img = output.squeeze().detach().cpu().numpy().astype(np.uint8)
+                    sr_imgs.append(sr_img)
+                    tmp_sid = sid
+
+                    # Save the image.
+                    output_dir = imgs_dir / patient
+                    if not output_dir.is_dir():
+                        output_dir.mkdir(parents=True)
+                    imsave(output_dir / f'{sid}_{fid}.png', sr_img)
+
+            batch_size = self.test_dataloader.batch_size
+            self._update_log(log, batch_size, loss, losses, metrics)
+            count += batch_size
+            trange.set_postfix(**dict((key, f'{value / count: .3f}') for key, value in log.items()))
+
+        # Save the results.
+        if self.export_prediction:
+            with open(csv_path, 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerows(results)
+
+        for key in log:
+            log[key] /= count
+        logging.info(f'Test log: {log}.')
+
     def _compute_losses(self, outputs, target):
         """Compute the losses.
         Args:
             outputs (list of torch.Tensor): The model outputs.
             target (torch.Tensor): The data target.
+
         Returns:
             losses (list of torch.Tensor): The computed losses.
         """
@@ -29,6 +112,7 @@ class AcdcSISRSRFBPredictor(AcdcSISRPredictor):
         Args:
             outputs (list of torch.Tensor): The model outputs.
             target (torch.Tensor): The data target.
+
         Returns:
             metrics (list of torch.Tensor): The computed metrics.
         """
