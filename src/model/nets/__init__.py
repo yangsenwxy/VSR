c9766f7a5c0acf27531228358f445c13e080f785
peter850706 <peter850706@gmail.com>
2019-07-27 15:24:26 +0800

    Add comments in /src/model/nets/__init__.py

--- src/model/nets/__init__.py
+++ src/model/nets/__init__.py
@@ -1,5 +1,10 @@
+# SISR
 from .srfb_net import SRFBNet
 from .edsr_net import EDSRNet
 from .drf_sisr_net import DRFSISRNet
+
+# MISR
 from .misr_duf_net import DUFNet
+
+# VSR
 from .drf_net import DRFNet
