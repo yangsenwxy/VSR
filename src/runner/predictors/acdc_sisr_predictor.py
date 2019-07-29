25c3c1c51ecd446b1ca355e3a5b27928806035cc
vic85821 <vic85821@gmail.com>
2019-07-30 00:15:27 +0800

    Fix the bugs when predict

--- src/runner/predictors/acdc_sisr_predictor.py
+++ src/runner/predictors/acdc_sisr_predictor.py
@@ -75,7 +75,7 @@ class AcdcSISRPredictor(BasePredictor):
                         self._dump_video(output_dir / video_name, sr_imgs)
                         sr_imgs = []
 
-                    output = self._denormalize(output)
+                    output = (self._denormalize(output) * 255).round()
                     sr_img = output.squeeze().detach().cpu().numpy().astype(np.uint8)
                     sr_imgs.append(sr_img)
                     tmp_sid = sid
