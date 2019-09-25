aeef2fda122b984239b8899db926ca0e1cfd53b2
vic85821 <vic85821@gmail.com>
2019-09-25 15:17:20 +0800

    Implement 3D VSR predictor

--- src/runner/predictors/acdc_vsr_predictor.py
+++ src/runner/predictors/acdc_vsr_predictor.py
@@ -15,16 +15,18 @@ from src.utils import denormalize
 class AcdcVSRPredictor(BasePredictor):
     """The ACDC predictor for the Video Super-Resolution.
     Args:
+        data_type (str): The data type. Default: `2d`.
         saved_dir (str): The directory to save the predicted videos, images and metrics (default: None).
         exported (bool): Whether to export the predicted video, images and metrics (default: False).
     """
-    def __init__(self, saved_dir=None, exported=False, **kwargs):
+    def __init__(self, data_type='2d', saved_dir=None, exported=False, **kwargs):
         super().__init__(**kwargs)
         if self.test_dataloader.batch_size != 1:
             raise ValueError(f'The testing batch size should be 1. Got {self.test_dataloader.batch_size}.')
 
         if exported:
             self.saved_dir = Path(saved_dir)
+        self.data_type = data_type
         self.exported = exported
         self._denormalize = functools.partial(denormalize, dataset='acdc')
 
@@ -54,44 +56,82 @@ class AcdcVSRPredictor(BasePredictor):
             batch = self._allocate_data(batch)
             inputs, targets, index = self._get_inputs_targets(batch)
             T = len(inputs)
+            if self.data_type == '3d':
+                D = inputs[0].shape[2]
             with torch.no_grad():
                 outputs = self.net(inputs)
                 losses = self._compute_losses(outputs, targets)
                 loss = (losses.mean(dim=0) * self.loss_weights).sum()
                 metrics = self._compute_metrics(outputs, targets)
+                if self.exported:                
+                    if self.data_type == '2d':
+                        lr_path, hr_path = self.test_dataloader.dataset.data[index]
+                        filename = lr_path.parts[-1].split('.')[0]
+                        patient, _, sid = filename.split('_')
 
-                if self.exported:
-                    lr_path, hr_path = self.test_dataloader.dataset.data[index]
-                    filename = lr_path.parts[-1].split('.')[0]
-                    patient, _, sid = filename.split('_')
+                        filename = filename.replace('2d+1d', '2d').replace('sequence', 'slice')
+                        for t, _losses, _metrics in zip(range(T), losses, metrics):
+                            _losses = [loss.item() for loss in _losses]
+                            _metrics = [metric.item() for metric in _metrics]
+                            results.append([filename + f'_frame{t+1:0>2d}', *_metrics, *_losses])
 
-                    filename = filename.replace('2d+1d', '2d').replace('sequence', 'slice')
-                    for t, _losses, _metrics in zip(range(T), losses, metrics):
-                        _losses = [loss.item() for loss in _losses]
-                        _metrics = [metric.item() for metric in _metrics]
-                        results.append([filename + f'_frame{t+1:0>2d}', *_metrics, *_losses])
+                        outputs = [self._denormalize(output) for output in outputs]
+                        sr_imgs = [output.squeeze().detach().cpu().numpy().astype(np.uint8)
+                                   for output in outputs]
 
-                    outputs = [self._denormalize(output) for output in outputs]
-                    sr_imgs = [output.squeeze().detach().cpu().numpy().astype(np.uint8)
-                               for output in outputs]
+                        # Save the video.
+                        output_dir = videos_dir / patient
+                        if not output_dir.is_dir():
+                            output_dir.mkdir(parents=True)
+                        self._dump_video(output_dir / f'{sid}.gif', sr_imgs)
 
-                    # Save the video.
-                    output_dir = videos_dir / patient
-                    if not output_dir.is_dir():
-                        output_dir.mkdir(parents=True)
-                    self._dump_video(output_dir / f'{sid}.gif', sr_imgs)
+                        # Save the image.
+                        output_dir = imgs_dir / patient
+                        if not output_dir.is_dir():
+                            output_dir.mkdir(parents=True)
+                        for t, sr_img in enumerate(sr_imgs):
+                            img_name = sid.replace('sequence', 'slice') + f'_frame{t+1:0>2d}.png'
+                            imsave(output_dir / img_name, sr_img)
+                    elif self.data_type == '3d':
+                        lr_path, hr_path = self.test_dataloader.dataset.data[index]
+                        filename = lr_path.parts[-1].split('.')[0]
+                        patient, _ = filename.split('_')
 
-                    # Save the image.
-                    output_dir = imgs_dir / patient
-                    if not output_dir.is_dir():
-                        output_dir.mkdir(parents=True)
-                    for t, sr_img in enumerate(sr_imgs):
-                        img_name = sid.replace('sequence', 'slice') + f'_frame{t+1:0>2d}.png'
-                        imsave(output_dir / img_name, sr_img)
+                        for i, (_losses, _metrics) in enumerate(zip(losses, metrics)):
+                            _losses = [loss.item() for loss in _losses]
+                            _metrics = [metric.item() for metric in _metrics]
+                            t, d = i % T, i // T
+                            results.append([f'{patient}_2d_slice{d+1:0>2d}_frame{t+1:0>2d}', *_metrics, *_losses])
+
+                        outputs = [self._denormalize(output) for output in outputs]
+                        
+                        # Save the video.
+                        output_dir = videos_dir / patient
+                        if not output_dir.is_dir():
+                            output_dir.mkdir(parents=True)
+                        for i in range(D):
+                            sr_imgs = [output[:, :, i].squeeze().detach().cpu().numpy().astype(np.uint8)
+                                   for output in outputs]
+                            self._dump_video(output_dir / f'sequence{i+1:0>2d}.gif', sr_imgs)
+
+                        # Save the image.
+                        output_dir = imgs_dir / patient
+                        if not output_dir.is_dir():
+                            output_dir.mkdir(parents=True)
+                        for i in range(D):
+                            sr_imgs = [output[:, :, i].squeeze().detach().cpu().numpy().astype(np.uint8)
+                                   for output in outputs]
+                            for t, sr_img in enumerate(sr_imgs):
+                                img_name = f'slice{i+1:0>2d}_frame{t+1:0>2d}.png'
+                                imsave(output_dir / img_name, sr_img)
 
             batch_size = self.test_dataloader.batch_size
-            self._update_log(log, batch_size, T, loss, losses, metrics)
-            count += batch_size * T
+            if self.data_type == '2d':
+                self._update_log(log, batch_size, T, loss, losses, metrics)
+                count += batch_size * T
+            elif self.data_type == '3d':
+                self._update_log(log, batch_size, T, loss, losses, metrics, D)
+                count += batch_size * T * D
             trange.set_postfix(**dict((key, f'{value / count: .3f}') for key, value in log.items()))
 
         # Save the results.
@@ -126,9 +166,17 @@ class AcdcVSRPredictor(BasePredictor):
             losses (torch.Tensor): The computed losses.
         """
         losses = []
-        for loss_fn in self.loss_fns:
-            losses.append(torch.stack([loss_fn(output, target) for output, target in zip(outputs, targets)]))
-        losses = torch.stack(losses, dim=1) # (T, #loss_fns)
+        if self.data_type == '2d':
+            for loss_fn in self.loss_fns:
+                losses.append(torch.stack([loss_fn(output, target) for output, target in zip(outputs, targets)]))
+            losses = torch.stack(losses, dim=1) # (T, #loss_fns)
+        elif self.data_type == '3d':
+            for loss_fn in self.loss_fns:
+                _losses = []
+                for d in range(outputs[0].shape[2]):
+                    _losses.extend([loss_fn(output[:, :, d], target[:, :, d]) for output, target in zip(outputs, targets)])    
+                losses.append(torch.stack(_losses))
+            losses = torch.stack(losses, dim=1) # (T*D, #loss_fns)
         return losses
 
     def _compute_metrics(self, outputs, targets):
@@ -144,26 +192,35 @@ class AcdcVSRPredictor(BasePredictor):
         targets = list(map(self._denormalize, targets))
 
         metrics = []
-        for metric_fn in self.metric_fns:
-            metrics.append(torch.stack([metric_fn(output, target) for output, target in zip(outputs, targets)]))
-        metrics = torch.stack(metrics, dim=1) # (T, #metric_fns)
+        if self.data_type == '2d':
+            for metric_fn in self.metric_fns:
+                metrics.append(torch.stack([metric_fn(output, target) for output, target in zip(outputs, targets)]))
+            metrics = torch.stack(metrics, dim=1) # (T, #metric_fns)
+        elif self.data_type == '3d':
+            for metric_fn in self.metric_fns:
+                _metrics = []
+                for d in range(outputs[0].shape[2]):
+                    _metrics.extend([metric_fn(output[:, :, d], target[:, :, d]) for output, target in zip(outputs, targets)])
+                metrics.append(torch.stack(_metrics))
+            metrics = torch.stack(metrics, dim=1) # (T*D, #metric_fns)
         return metrics
 
-    def _update_log(self, log, batch_size, T, loss, losses, metrics):
+    def _update_log(self, log, batch_size, T, loss, losses, metrics, D=1):
         """Update the log.
         Args:
             log (dict): The log to be updated.
             batch_size (int): The batch size.
             T (int): The total number of the frames.
+            D (int): The total number of the slices.
             loss (torch.Tensor): The weighted sum of the computed losses.
             losses (sequence of torch.Tensor): The computed losses.
             metrics (sequence of torch.Tensor): The computed metrics.
         """
-        log['Loss'] += loss.item() * batch_size * T
+        log['Loss'] += loss.item() * batch_size * T * D
         for loss_fn, loss in zip(self.loss_fns, losses.mean(dim=0)):
-            log[loss_fn.__class__.__name__] += loss.item() * batch_size * T
+            log[loss_fn.__class__.__name__] += loss.item() * batch_size * T * D
         for metric_fn, metric in zip(self.metric_fns, metrics.mean(dim=0)):
-            log[metric_fn.__class__.__name__] += metric.item() * batch_size * T
+            log[metric_fn.__class__.__name__] += metric.item() * batch_size * T * D
 
     def _dump_video(self, path, imgs):
         """To dump the video by concatenate the images.
