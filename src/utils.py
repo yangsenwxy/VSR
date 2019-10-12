019f0d36a1342e5eaaf95a0a756e0869656e91d5
peter850706 <peter850706@gmail.com>
2019-10-12 21:15:08 +0800

    Update the mean and std

--- src/utils.py
+++ src/utils.py
@@ -3,18 +3,18 @@ def denormalize(imgs, dataset):
     Args:
         imgs (torch.Tensor) (N, C, H, W): The images to be denormalized.
         dataset (str): The name of the dataset.
-        
+
     Returns:
         imgs (torch.Tensor) (N, C, H, W): The denormalized images.
     """
     if dataset not in ['acdc', 'dsb15']:
         raise ValueError(f"The name of the dataset should be 'acdc' or 'dsb15'. Got {dataset}.")
-    
+
     if dataset == 'acdc':
         mean, std = 54.089, 48.084
     elif dataset == 'dsb15':
-        mean, std = 51.174, 52.527
-        
+        mean, std = 51.193, 52.671
+
     imgs = imgs.clone()
     imgs = (imgs * std + mean).round().clamp(0, 255)
     return imgs
